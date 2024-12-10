8194695159dacc8d56a0ff80037066f79e50203b
rich@wight <r.j.henry@gmail.com>
2024-12-10 14:18:59 +0000

    Going to look at player state

--- scene/mob.gd
+++ scene/mob.gd
@@ -1,4 +1,4 @@
-class_name Mob extends Node2D
+@tool class_name Mob extends Node2D
 
 @export var base_frame: int = 1
 
@@ -6,4 +6,6 @@ var recycle: Callable
 var destroyed: Callable
 
 func _ready() -> void:
+	if Engine.is_editor_hint():
+		print("In editor")
 	$Sprite2D.frame = base_frame
